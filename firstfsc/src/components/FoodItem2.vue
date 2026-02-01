c19092dd37c17b9c68fb5633b8bb2da12518c995
Eunice B. Nipas <ebnipas@student.apc.edu.ph>
2026-02-02 02:22:49 +0800

    fixed error

--- firstfsc/src/components/FoodItem2.vue
+++ firstfsc/src/components/FoodItem2.vue
@@ -1,5 +1,5 @@
 <template>
-  <divclass="berries">
+  <div class="berries">
     <h2>{{ name }}</h2>
     <p>{{ message }}</p>
   </div>
@@ -10,20 +10,20 @@ export default {
   data() {
     return {
       name: 'Little Lad',
-      message: 'Im a little lad who loves berries and cream 🕺  I hope u get my reference 🙏'
+      message: 'Im a little lad who loves berries and cream.   I hope u get my reference '
     }
   }
 };
 </script>
 
 <style>
-
   .berries {
     border: dashed black 1px;
     display: inline-block;
     margin: 10px;
     padding: 10px;
-    background-color: blue;
+    background-color: rgb(25, 3, 138);
   }
-  
-  </style>
+
+
+</style>
